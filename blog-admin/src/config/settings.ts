1703f8563bbadb8987826ed6093ad548d7443284
Binfinity <binfinity@qq.com>
2025-04-15 17:14:42 +0800

    Initial commit

---
+++ blog-admin/src/config/settings.ts
@@ -0,0 +1,15 @@
+interface Settings {
+  title: string;
+  shortTitle: string;
+  description: string;
+  repository: string;
+}
+
+const settings: Settings = {
+  title: 'Infinity.log',
+  shortTitle: 'Infinity.log博客管理系统',
+  description: 'By Binfinity',
+  repository: 'https://github.com/AaaBinfinity/Infinity.log.git'
+}
+
+export default settings 
---
+++ blog-admin/src/layouts/components/Sidebar/Logo.vue
@@ -0,0 +1,32 @@
+<template>
+  <img
+      class="logo-img"
+      :src="logoUrl"
+      :width="size"
+      :height="size"
+      alt="Logo"
+  />
+</template>
+
+<script setup lang="ts">
+import logoUrl from '@/assets/logo.png'
+
+defineProps({
+  size: {
+    type: [Number, String],
+    default: 32
+  },
+})
+</script>
+
+
+
+<style scoped>
+.logo-img {
+  transition: transform 0.3s ease;
+}
+
+.logo-img:hover {
+  transform: scale(1.1);
+}
+</style> 
